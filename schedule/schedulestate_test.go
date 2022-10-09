844bc13f7813c661e3f7df837cb38080b2becae4
Dylan Scott <drrscott@hotmail.com>
2022-10-09 01:10:25 -0500

    Update schedulestate_test.go

--- schedule/schedulestate_test.go
+++ schedule/schedulestate_test.go
@@ -3,5 +3,9 @@ packagr schedule_test
 import "testing"
 
 func TestSchedule_Next(t *testing.T) {
-
+    expected := "Running"
+    actual := ScheduleState.Running.String()
+    if expected != actual {
+        t.Errorf("Expected %s do not match actual %s", expected, actual)
+    }
 }
