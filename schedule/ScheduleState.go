29470bf01aa52e4c5870ef12f8aeff1ac2380029
Dylan Scott <drrscott@hotmail.com>
2022-10-08 23:24:45 -0500

    Schedule state enum type

---
+++ schedule/ScheduleState.go
@@ -0,0 +1,31 @@
+
+//Package schedule is meant to list the states a schedule can be in as well as the times jobs will run
+package schedule
+
+//ScheduleState will hold the 
+type ScheduleState int64
+
+const (
+	Running ScheduleState = iota
+	Delayed
+	Stopped
+	Init
+  Unknown
+)
+
+//Function String will return a readable string of the int code of the schedule's state
+func (state ScheduleState) String() string {
+	switch state {
+	case Running:
+		return "Running"
+	case Delayed:
+		return "Delayed"
+	case Stopped:
+		return "Stopped"
+	case Init:
+		return "Init"
+ 	case Unknown:
+		return "Unknown"
+	}
+	return "Unknown"
+}
