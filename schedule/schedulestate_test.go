35c26967a21608401f15fbba3cc1d6cf7efcf588
Dylan Scott <drrscott@hotmail.com>
2022-10-09 21:55:41 -0500

    Update schedulestate_test.go

--- schedule/schedulestate_test.go
+++ schedule/schedulestate_test.go
@@ -5,7 +5,7 @@ import ss "scheduleorch/schedule"
 
 func TestSchedule_Next(t *testing.T) {
     expected := "Running"
-    actual := ss.ScheduleState.Running.String()
+    actual := ss.Running.String()
     if expected != actual {
         t.Errorf("Expected %s do not match actual %s", expected, actual)
     }
@@ -13,7 +13,7 @@ func TestSchedule_Next(t *testing.T) {
 
 func TestSchedule_Unknown(t *testing.T) {
     expected := "Unknown"
-    actual := ss.ScheduleState.Unknown.String()
+    actual := ss.Unknown.String()
     if expected != actual {
         t.Errorf("Expected %s do not match actual %s", expected, actual)
     }
