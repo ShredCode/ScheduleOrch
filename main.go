5b94abb91a4ccb62caccfda1cd5ddf410ee43caa
Dylan Scott <drrscott@hotmail.com>
2022-10-10 09:48:46 -0500

    Update main.go

--- main.go
+++ main.go
@@ -1,11 +1,7 @@
 package main
-import "fmt"
-import "github.com/spf13/cobra"
+
+import schedulecli/cmd/schedulecli"
 
 func main() {
-    fmt.Println("Welcome to the scheudle cli...")
-    fmt.Println("The intention is for this tooling to work with schedules and report their status")
-    
-    
-
+    schedulecli.Execute()
 }
